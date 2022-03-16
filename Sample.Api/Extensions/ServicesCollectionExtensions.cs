2e107d4dc78329af06190bd9251dc7e3526991c1
Paulo Pitte <paulopitte@gmail.com>
2022-03-16 15:43:08 -0300

    Adicionar arquivos de projeto.

---
+++ Sample.Api/Extensions/ServicesCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using MassTransit;
+using Sample.Twitch.Components;
+using Sample.Twitch.Components.Consumers;
+using Sample.Twitch.Contract;
+
+namespace Sample.Api.Extensions
+{
+    public static class ServicesCollectionExtensions
+    {
+        public static IServiceCollection AddBus(this IServiceCollection services)
+        {
+            services.AddMediator(m =>
+            {
+                m.AddConsumer<SubmitOrderConsumer>();
+                m.AddRequestClient<SubmitOrder>();
+                m.ConfigureMediator((context, mcfg) =>
+                {
+                    mcfg.UseSendFilter(typeof(ValidateOrderFilter<>), context);
+                });
+            });
+
+            return services;
+        }
+    }
+}
---
+++ Sample.Twitch.Components/ValidateOrderFilter.cs
@@ -0,0 +1,35 @@
+﻿
+using GreenPipes;
+using MassTransit;
+using Sample.Twitch.Contract;
+
+namespace Sample.Twitch.Components
+{
+    public class ValidateOrderFilter<T> :
+        IFilter<SendContext<T>>
+        where T : class
+    {
+        public void Probe(ProbeContext context)
+        {
+         
+        }
+
+        /// <summary>
+        /// Middleware
+        /// O MassTransit Mediator é construído usando os mesmos componentes usados ​​para criar um barramento, 
+        /// o que significa que todos os mesmos componentes de middleware podem ser configurados.Por exemplo, 
+        /// para configurar o pipeline do Mediador, como adicionar um filtro com escopo, consulte o exemplo abaixo.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="next"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public Task Send(SendContext<T> context, IPipe<SendContext<T>> next)
+        {
+            if (context.Message is SubmitOrder orderMessage && orderMessage.CustomerName.Contains("TEST"))
+                throw new ArgumentException("The CUSTOMERNAME must not be TEST");
+
+            return next.Send(context);
+        }
+    }
+}
