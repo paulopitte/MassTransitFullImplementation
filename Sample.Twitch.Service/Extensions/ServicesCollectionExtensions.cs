33a3ec3c4e418f73488a9f8226fe535e39088726
Paulo Pitte <paulopitte@gmail.com>
2022-03-16 16:00:21 -0300

    start imp

---
+++ Sample.Twitch.Service/Extensions/ServicesCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Sample.Twitch.Service.Extensions
+{
+    public static class ServicesCollectionExtensions
+    {
+        public static IServiceCollection AddMessageBroker(this IServiceCollection services)
+        {
+            services.AddMediator(m =>
+            {
+                //m.AddConsumer<SubmitOrderConsumer>();
+                //m.AddRequestClient<SubmitOrder>();
+                //m.ConfigureMediator((context, mcfg) =>
+                //{
+                //    mcfg.UseSendFilter(typeof(ValidateOrderFilter<>), context);
+                //});
+            });
+
+            return services;
+        }
+    }
+}
